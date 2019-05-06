8de85e3b60538d181cae27186beab44d2ac065f2
carlos.reis <carlos.reis@semantix.com.br>
2019-05-06 19:31:54 -0300

    Updating setup file

--- carlosReisData_setup.sh
+++ carlosReisData_setup.sh
@@ -27,29 +27,30 @@ pip3 install glob3
 pip3 install pandas
 
 # Criação das pastas locais
-mkdir carlosReis
-mkdir carlosReis/bin
-mkdir carlosReis/crawler_crypto
-mkdir carlosReis/crawler_crypto/processados
-mkdir carlosReis/crawler_crypto/consolidados
-mkdir carlosReis/crawler_crypto/consolidados/transferidos
-mkdir carlosReis/crawler_dolar
-mkdir carlosReis/crawler_dolar/transferidos
-mkdir carlosReis/processados_json
-mkdir carlosReis/processados_json/indexados
+mkdir -p carlosReis
+mkdir -p carlosReis/bin
+mkdir -p carlosReis/crawler_crypto
+mkdir -p carlosReis/crawler_crypto/processados
+mkdir -p carlosReis/crawler_crypto/consolidados
+mkdir -p carlosReis/crawler_crypto/consolidados/transferidos
+mkdir -p carlosReis/crawler_dolar
+mkdir -p carlosReis/crawler_dolar/transferidos
+mkdir -p carlosReis/processados_json
+mkdir -p carlosReis/processados_json/indexados
 
 # Criação das pastas no hdfs
-hdfs dfs -mkdir /user/carlosReis
-hdfs dfs -mkdir /user/carlosReis/input
-hdfs dfs -mkdir /user/carlosReis/input/processados
-hdfs dfs -mkdir /user/carlosReis/output
-hdfs dfs -mkdir /user/carlosReis/output/transferidos
+hdfs dfs -mkdir -p /user/carlosReis
+hdfs dfs -mkdir -p /user/carlosReis/input
+hdfs dfs -mkdir -p /user/carlosReis/input/processados
+hdfs dfs -mkdir -p /user/carlosReis/output
+hdfs dfs -mkdir -p /user/carlosReis/output/transferidos
 
 # Download dos scripts no github, descompactação e realocação dos scripts
 wget --no-check-certificate --content-disposition https://github.com/carlosborgesreis/desafio_semantix_scripts/archive/master.zip
 unzip desafio_semantix_scripts-master.zip
 mv desafio_semantix_scripts-master/*.py carlosReis/bin
 mv desafio_semantix_scripts-master/*.sh carlosReis/bin
+mv desafio_semantix_scripts-master/*.jar carlosReis/bin
 rm desafio_semantix_scripts-master.zip
 rm -r desafio_semantix_scripts-master
 
@@ -57,14 +58,14 @@ rm -r desafio_semantix_scripts-master
 # Crawler das criptomoedas a cada 20 minutos
 (crontab -l 2>/dev/null; echo "*/20 * * * * python3 $PWD/carlosReis/bin/crypto_crawler.py $PWD") | crontab -
 # Consolidação dos dados das criptomoedas uma vez por dia às 12:01
-(crontab -l 2>/dev/null; echo "5 12 */1 * * python3 $PWD/carlosReis/bin/crypto_join_copy.py $PWD") | crontab -
-# Crawler do dólar uma vez por dia às 12:00
-(crontab -l 2>/dev/null; echo "0 12 */1 * * python3 $PWD/carlosReis/bin/dolar_crawler.py $PWD") | crontab -
+(crontab -l 2>/dev/null; echo "5 18 */1 * * python3 $PWD/carlosReis/bin/crypto_join_copy.py $PWD") | crontab -
+# Crawler do dólar uma vez por dia às 18:00
+(crontab -l 2>/dev/null; echo "0 18 */1 * * python3 $PWD/carlosReis/bin/dolar_crawler.py $PWD") | crontab -
 
 # Adicionando permissões ao script que envia dados para o hdfs
 chmod +x carlosReis/bin/carlosReisData_send_to_hdfs.sh
 # Adicionando os scripts de envio ao hdfs e processamento dos dados enviados ao crontab
-(crontab -l 2>/dev/null; echo "10 12 */1 * * $PWD/carlosReis/bin/carlosReisData_send_to_hdfs.sh $PWD") | crontab -
+(crontab -l 2>/dev/null; echo "10 18 */1 * * $PWD/carlosReis/bin/carlosReisData_send_to_hdfs.sh $PWD") | crontab -
 
 
 
