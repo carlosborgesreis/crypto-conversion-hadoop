54eb718000ca6d70c235e77bf97fba6559c6f1ab
carlos.reis <carlos.reis@semantix.com.br>
2019-05-07 17:40:13 -0300

    one more

--- carlosReisData_send_to_hdfs.sh
+++ carlosReisData_send_to_hdfs.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 /usr/bin/python3 $1/carlosReis/bin/dolar_zip_transferidos.py $1
-cp $1/carlosReis/crawler_crypto/processados/processados.zip $1/carlosReis/crawler_crypto/consolidados/transferidos/transferidos.zip
 hdfs dfs -moveFromLocal $1/carlosReis/crawler_crypto/consolidados/crypto_data.csv /user/carlosReis/input
 hdfs dfs -moveFromLocal $1/carlosReis/crawler_dolar/dolar_data.csv /user/carlosReis/input
-
 
 # Processa os dados no hdfs e os pega de volta no final
 spark-submit --master local[*] $1/carlosReis/bin/processamento_spark.jar
 
-hdfs dfs -get /user/carlosReis/output/*.json $1/carlosReis/processados_json
-
+hdfs dfs -get "/user/carlosReis/output/*.json" $1/carlosReis/processados_json
+hdfs dfs -rm "/user/carlosReis/output/*.json"
--- dolar_zip_transferidos.py
+++ dolar_zip_transferidos.py
@@ -7,11 +7,11 @@ wdir = sys.argv[1]
 filename_dolar = f"{wdir}/carlosReis/crawler_dolar/dolar_data.csv"
 filename_crypto = f"{wdir}/carlosReis/crawler_crypto/consolidados/crypto_data.csv"
 
-with zipfile.ZipFile(f'{wdir}carlosReis/crawler_dolar/transferidos/transferidos.zip', 'a') as myzip:
+with zipfile.ZipFile(f'{wdir}/carlosReis/crawler_dolar/transferidos/transferidos.zip', 'a') as myzip:
     myzip.write(filename_dolar)
 
 # Cria uma cópia zipada na pasta transferidos
-with zipfile.ZipFile(f"{wdir}/carlosReis/crawler_crypto/transferidos/transferidos.zip", "a") as myzip:
+with zipfile.ZipFile(f"{wdir}/carlosReis/crawler_crypto/consolidados/transferidos/transferidos.zip", "a") as myzip:
     myzip.write(filename_crypto)
 
 os.remove(f"{wdir}/carlosReis/crawler_crypto/*.csv")
