282fe2d19d7cfd8c006b9a482273c2b13b2a8a06
carlos.reis <carlos.reis@semantix.com.br>
2019-05-07 16:06:05 -0300

    Rewriting some commands

--- carlosReisData_send_to_hdfs.sh
+++ carlosReisData_send_to_hdfs.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
-cp $1carlosReis/crawler_crypto/processados/processados.zip $1carlosReis/crawler_crypto/consolidados/transferidos/transferidos.zip
-/usr/bin/python3 $1/carlosReis/bin/dolar_zip_transferidos.py
+/usr/bin/python3 dolar_zip_transferidos.py
+cp carlosReis/crawler_crypto/processados/processados.zip carlosReis/crawler_crypto/consolidados/transferidos/transferidos.zip
 hdfs dfs -moveFromLocal $1/carlosReis/crawler_crypto/consolidados/crypto_data.csv /user/carlosReis/input
 hdfs dfs -moveFromLocal $1/carlosReis/crawler_dolar/dolar_data.csv /user/carlosReis/input
 
+
 # Processa os dados no hdfs e os pega de volta no final
 spark-submit --class processamento_spark --master local[*] $1/carlosReis/bin/processamento_spark.jar
-
-hdfs dfs -get /user/carlosReis/output/processado_data.json $1/carlosReis/processados_json/
+hdfs dfs -rm /user/carlosReis/input/*.csv
+hdfs dfs -get /user/carlosReis/output/*.json $1/carlosReis/processados_json
+hdfs dfs -rm /user/carlosReis/output/*.json
