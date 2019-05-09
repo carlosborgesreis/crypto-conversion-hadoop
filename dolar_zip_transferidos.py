affa2d8e179170554ed3a3560a893d0a7f2161bc
carlos.reis <carlos.reis@semantix.com.br>
2019-05-09 01:11:59 -0300

    i am inevitable

--- dolar_zip_transferidos.py
+++ dolar_zip_transferidos.py
@@ -1,6 +1,7 @@
 import zipfile
 import os
 import sys
+from os.path import basename
 
 wdir = sys.argv[1]
 
@@ -8,8 +9,8 @@ filename_dolar = f"{wdir}/carlosReis/crawler_dolar/dolar_data.csv"
 filename_crypto = f"{wdir}/carlosReis/crawler_crypto/consolidados/crypto_data.csv"
 
 with zipfile.ZipFile(f'{wdir}/carlosReis/crawler_dolar/transferidos/transferidos.zip', 'a') as myzip:
-    myzip.write(filename_dolar)
+    myzip.write(filename_dolar, basename(filename_dolar))
 
 # Cria uma cópia zipada na pasta transferidos
 with zipfile.ZipFile(f"{wdir}/carlosReis/crawler_crypto/consolidados/transferidos/transferidos.zip", "a") as myzip:
-    myzip.write(filename_crypto)
+    myzip.write(filename_crypto, basename(filename_crypto))
