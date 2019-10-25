e759541269f6bcc6fbcdf18faba73054074465fb
Carlos Borges Reis <carlosborges161@gmail.com>
2019-10-25 17:37:56 -0300

    Update carlosReisData_setup.sh

--- carlosReisData_setup.sh
+++ carlosReisData_setup.sh
@@ -1,68 +1,79 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
 
 echo "Instalando o python 3, o pip 3 e o unzip com apt-get"
-apt-get update > /dev/null 2>&1
-apt-get install zip unzip -y > /dev/null 2>&1
-apt-get install python3 -y > /dev/null 2>&1
-apt-get install python3-pip -y > /dev/null 2>&1
+{ 
+    sudo yum install -y https://centos7.iuscommunity.org/ius-release.rpm
+    sudo yum install -y python36u python36u-devel python36u-pip
+    sudo yum install -y unzip    
+} &> /dev/null
 
 echo "Instalando os componentes necessários no processamento: requests, beautifulsoup4, glob3, pandas"
-pip3 install requests > /dev/null 2>&1
-pip3 install beautifulsoup4 > /dev/null 2>&1
-pip3 install glob3 > /dev/null 2>&1
-pip3 install pandas > /dev/null 2>&1
-pip3 install elasticsearch > /dev/null 2>&1
+{
+    sudo pip3 install requests
+    sudo pip3 install beautifulsoup4
+    sudo pip3 install glob3
+    sudo pip3 install pandas
+    sudo pip3 install elasticsearch
+} &> /dev/null
 
 echo "Criando os diretórios locais"
-mkdir -p carlosReis > /dev/null 2>&1
-mkdir -p carlosReis/bin > /dev/null 2>&1
-mkdir -p carlosReis/crawler_crypto > /dev/null 2>&1
-mkdir -p carlosReis/crawler_crypto/processados > /dev/null 2>&1
-mkdir -p carlosReis/crawler_crypto/consolidados > /dev/null 2>&1
-mkdir -p carlosReis/crawler_crypto/consolidados/transferidos > /dev/null 2>&1
-mkdir -p carlosReis/crawler_dolar > /dev/null 2>&1
-mkdir -p carlosReis/crawler_dolar/transferidos > /dev/null 2>&1
-mkdir -p carlosReis/processados_json > /dev/null 2>&1
-mkdir -p carlosReis/processados_json/indexados > /dev/null 2>&1
+mkdir -p carlosReis
+mkdir -p carlosReis/bin
+mkdir -p carlosReis/crawler_crypto
+mkdir -p carlosReis/crawler_crypto/processados
+mkdir -p carlosReis/crawler_crypto/consolidados
+mkdir -p carlosReis/crawler_crypto/consolidados/transferidos
+mkdir -p carlosReis/crawler_dolar
+mkdir -p carlosReis/crawler_dolar/transferidos
+mkdir -p carlosReis/processados_json
+mkdir -p carlosReis/processados_json/indexados
 
 echo "Criando os diretórios no hdfs"
-hdfs dfs -mkdir -p /user/carlosReis > /dev/null 2>&1
-hdfs dfs -mkdir -p /user/carlosReis/input > /dev/null 2>&1
-hdfs dfs -mkdir -p /user/carlosReis/input/processados > /dev/null 2>&1
-hdfs dfs -mkdir -p /user/carlosReis/output > /dev/null 2>&1
-hdfs dfs -mkdir -p /user/carlosReis/output/transferidos > /dev/null 2>&1
+{
+    hdfs dfs -mkdir -p carlosReis
+    hdfs dfs -mkdir -p carlosReis/input
+    hdfs dfs -mkdir -p carlosReis/input/processados
+    hdfs dfs -mkdir -p carlosReis/output
+    hdfs dfs -mkdir -p carlosReis/output/transferidos
+} &> /dev/null
 
 echo "Fazendo download dos scripts no github, descompactando e realocando os scripts"
-wget --no-check-certificate --content-disposition https://github.com/carlosborgesreis/desafio_semantix_scripts/archive/master.zip > /dev/null 2>&1
-unzip desafio_semantix_scripts-master.zip > /dev/null 2>&1
-mv desafio_semantix_scripts-master/*.py carlosReis/bin
-mv desafio_semantix_scripts-master/*.sh carlosReis/bin
-mv desafio_semantix_scripts-master/*.jar carlosReis/bin
-chmod +x carlosReis/bin/*
-rm desafio_semantix_scripts-master.zip
-rm -r desafio_semantix_scripts-master
+{
+    wget --no-check-certificate --content-disposition https://github.com/carlosborgesreis/desafio_semantix_scripts/archive/master.zip
+    unzip desafio_semantix_scripts-master.zip
+    mv desafio_semantix_scripts-master/*.py carlosReis/bin
+    mv desafio_semantix_scripts-master/*.sh carlosReis/bin
+    mv desafio_semantix_scripts-master/*.jar carlosReis/bin
+    chmod +x carlosReis/bin/*
+    rm desafio_semantix_scripts-master.zip
+    rm -r desafio_semantix_scripts-master
+} &> /dev/null
+
+touch cronfile
 
 echo "Adicionando os crawlers ao crontab em ordem de execução, uma por minuto"
 # Crawler das criptomoedas a cada 20 minutos
-(crontab -l 2>&1; echo "*/20 * * * * cd $PWD && /usr/bin/python3 $PWD/carlosReis/bin/crypto_crawler.py $PWD") | crontab -
+echo "*/20 * * * * python3 ~/carlosReis/bin/crypto_crawler.py ~" >> cronfile
 # Consolidação dos dados das criptomoedas uma vez por dia às 12:01
-(crontab -l 2>&1; echo "1 23 * * * cd $PWD && /usr/bin/python3 $PWD/carlosReis/bin/crypto_join_copy.py $PWD") | crontab -
+echo "1 23 * * * python3 ~/carlosReis/bin/crypto_join_copy.py ~" >> cronfile
 # Crawler do dólar uma vez por dia às 14:00
-(crontab -l 2>&1; echo "0 23 * * * cd $PWD && /usr/bin/python3 $PWD/carlosReis/bin/dolar_crawler.py $PWD") | crontab -
+echo "0 23 * * * python3 ~/carlosReis/bin/dolar_crawler.py ~" >> cronfile
 
 # Envio ao hdfs e processamento dos dados enviados
-(crontab -l 2>&1; echo "2 23 * * * cd $PWD && /usr/bin/python3 $PWD/carlosReis/bin/dolar_zip_transferidos.py $PWD >> $PWD/carlosReis/log.log") | crontab -
-(crontab -l 2>&1; echo "3 23 * * * cd $PWD && /usr/local/hadoop/bin/hdfs dfs -put $PWD/carlosReis/crawler_crypto/consolidados/crypto_data.csv /user/carlosReis/input >> $PWD/carlosReis/log.log") | crontab -
-(crontab -l 2>&1; echo "3 23 * * * cd $PWD && /usr/local/hadoop/bin/hdfs dfs -put $PWD/carlosReis/crawler_dolar/dolar_data.csv /user/carlosReis/input >> $PWD/carlosReis/log.log") | crontab -
-(crontab -l 2>&1; echo "3 23 * * * cd $PWD && /usr/local/hadoop/bin/hdfs dfs -put $PWD/carlosReis/crawler_crypto/consolidados/crypto_data.csv /user/carlosReis/input/processados >> $PWD/carlosReis/log.log") | crontab -
-(crontab -l 2>&1; echo "3 23 * * * cd $PWD && /usr/local/hadoop/bin/hdfs dfs -put $PWD/carlosReis/crawler_dolar/dolar_data.csv /user/carlosReis/input/processados >> $PWD/carlosReis/log.log") | crontab -
-(crontab -l 2>&1; echo "4 23 * * * cd $PWD && rm $PWD/carlosReis/crawler_crypto/consolidados/crypto_data.csv >> $PWD/carlosReis/log.log") | crontab -
-(crontab -l 2>&1; echo "4 23 * * * cd $PWD && rm $PWD/carlosReis/crawler_dolar/dolar_data.csv >> $PWD/carlosReis/log.log") | crontab -
+echo "2 23 * * * python3 ~/carlosReis/bin/dolar_zip_transferidos.py ~" >> cronfile
+echo "3 23 * * * hdfs dfs -put ~/carlosReis/crawler_crypto/consolidados/crypto_data.csv carlosReis/input" >> cronfile
+echo "3 23 * * * hdfs dfs -put ~/carlosReis/crawler_dolar/dolar_data.csv carlosReis/input" >> cronfile
+echo "3 23 * * * hdfs dfs -put ~/carlosReis/crawler_crypto/consolidados/crypto_data.csv carlosReis/input/processados" >> cronfile
+echo "3 23 * * * hdfs dfs -put ~/carlosReis/crawler_dolar/dolar_data.csv carlosReis/input/processados" >> cronfile
+echo "4 23 * * * rm ~/carlosReis/crawler_crypto/consolidados/crypto_data.csv" >> cronfile
+echo "4 23 * * * rm ~/carlosReis/crawler_dolar/dolar_data.csv" >> cronfile
 
 # Processa os dados no hdfs e os pega de volta no final
-(crontab -l 2>&1; echo "5 23 * * * cd $PWD && /usr/local/spark/bin/spark-submit --master local[*] $PWD/carlosReis/bin/processamento_spark.jar >> $PWD/carlosReis/log.log") | crontab -
-(crontab -l 2>&1; echo "6 23 * * * cd $PWD && /usr/local/hadoop/bin/hdfs dfs -get \"/user/carlosReis/output/*.json\" $PWD/carlosReis/processados_json >> $PWD/carlosReis/log.log") | crontab -
-(crontab -l 2>&1; echo "7 23 * * * cd $PWD && /usr/local/hadoop/bin/hdfs dfs -rm -r \"/user/carlosReis/output/*.json\" >> $PWD/carlosReis/log.log") | crontab -
-(crontab -l 2>&1; echo "8 23 * * * cd $PWD && /usr/bin/python3 $PWD/carlosReis/bin/rename_json_file.py $PWD >> $PWD/carlosReis/log.log") | crontab -
-(crontab -l 2>&1; echo "9 23 * * * cd $PWD && /usr/bin/python3 $PWD/carlosReis/bin/create_index.py $PWD >> $PWD/carlosReis/log.log") | crontab -
+echo "5 23 * * * spark-submit --master local[*] ~/carlosReis/bin/processamento_spark.jar" >> cronfile
+echo "6 23 * * * hdfs dfs -get \"carlosReis/output/*.json\" ~/carlosReis/processados_json" >> cronfile
+echo "7 23 * * * hdfs dfs -rm -r \"carlosReis/output/*.json\"" >> cronfile
+echo "8 23 * * * python3 ~/carlosReis/bin/rename_json_file.py ~" >> cronfile
+echo "9 23 * * * python3 ~/carlosReis/bin/create_index.py ~" >> cronfile
 
+crontab cronfile
